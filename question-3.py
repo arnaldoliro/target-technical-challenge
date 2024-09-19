3b9696942b758eeb7948014b3f4f87ae0cf83bf4
Arnaldo Liro <dinholiro@gmail.com>
2024-09-19 19:03:59 -0300

    Finalização da questão 3

---
+++ question-3.py
@@ -0,0 +1,12 @@
+# Declaração das variaveis
+indice = 12
+soma = 0
+k = 1
+
+# Função do enquanto pedida na atividade
+while k < indice:
+    k = k + 1
+    soma = soma + k
+
+# Impressão da variavel soma
+print(soma)
